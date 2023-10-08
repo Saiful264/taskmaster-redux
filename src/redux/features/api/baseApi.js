d606e475fcc47d3348c23f80e683b37159fc3c84
Saiful264 <saifulislamx264@gmail.com>
2023-10-08 23:23:34 +0600

    implement RTK query in the project

---
+++ src/redux/features/api/baseApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const baseApi = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/" }),
+  endpoints: (builder) => ({
+    getTasks: builder.query({
+      query: () => "/tasks",
+    }),
+    updateTask: builder.mutation({
+      query: ({ id, data }) => ({
+        url: `/tasks/${id}`,
+        method: "PATCH",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const { useGetTasksQuery, useUpdateTaskMutation } = baseApi;
+
+export default baseApi;
