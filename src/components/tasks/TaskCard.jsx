952270933236c25026a3ba2d0430155d31898403
Saiful264 <saifulislamx264@gmail.com>
2023-10-09 20:35:12 +0600

    addtask endpoint and tag type been added

--- src/components/tasks/TaskCard.jsx
+++ src/components/tasks/TaskCard.jsx
@@ -1,29 +1,42 @@
-import { ArrowRightIcon, TrashIcon } from '@heroicons/react/24/outline';
-import { useDispatch } from 'react-redux';
-import {
-  removeTask,
-  updateStatus,
-} from '../../redux/features/tasks/tasksSlice';
+import { ArrowRightIcon, TrashIcon } from "@heroicons/react/24/outline";
+import { useUpdateTaskMutation } from "../../redux/features/api/baseApi";
 
 const TaskCard = ({ task }) => {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
+  const [updateTask, { data, error }] = useUpdateTaskMutation();
+
+  console.log("data", data);
+  console.log("error", error);
+
+  const handleUpdate = (id, updatedStatus) => {
+    const data = {
+      status: updatedStatus,
+    };
+
+    const options = {
+      id: id,
+      data: data,
+    };
+
+    updateTask(options);
+  };
 
   let updatedStatus;
 
-  if (task.status === 'pending') {
-    updatedStatus = 'running';
-  } else if (task.status === 'running') {
-    updatedStatus = 'done';
+  if (task.status === "pending") {
+    updatedStatus = "running";
+  } else if (task.status === "running") {
+    updatedStatus = "done";
   } else {
-    updatedStatus = 'archive';
+    updatedStatus = "archive";
   }
   return (
     <div className="bg-secondary/10 rounded-md p-5">
       <h1
         className={`text-lg font-semibold mb-3 ${
-          task.priority === 'high' ? 'text-red-500' : ' '
-        } ${task.priority === 'medium' ? 'text-yellow-500' : ' '} ${
-          task.priority === 'low' ? 'text-green-500' : ' '
+          task.priority === "high" ? "text-red-500" : " "
+        } ${task.priority === "medium" ? "text-yellow-500" : " "} ${
+          task.priority === "low" ? "text-green-500" : " "
         }`}
       >
         {task?.title}
@@ -33,15 +46,10 @@ const TaskCard = ({ task }) => {
       <div className="flex justify-between mt-3">
         <p>{task?.date}</p>
         <div className="flex gap-3">
-          <button onClick={() => dispatch(removeTask(task.id))} title="Delete">
+          <button onClick={() => console.log("remove")} title="Delete">
             <TrashIcon className="h-5 w-5 text-red-500" />
           </button>
-          <button
-            onClick={() =>
-              dispatch(updateStatus({ id: task.id, status: updatedStatus }))
-            }
-            title="Update Status"
-          >
+          <button onClick={() => handleUpdate(task._id, updatedStatus)} title="Update Status">
             <ArrowRightIcon className="h-5 w-5 text-primary" />
           </button>
         </div>
--- src/redux/features/api/baseApi.js
+++ src/redux/features/api/baseApi.js
@@ -3,9 +3,11 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/" }),
+  tagTypes: ["Tasks"],
   endpoints: (builder) => ({
     getTasks: builder.query({
       query: () => "/tasks",
+      providesTags: ["Tasks"],
     }),
     updateTask: builder.mutation({
       query: ({ id, data }) => ({
@@ -13,10 +15,20 @@ const baseApi = createApi({
         method: "PATCH",
         body: data,
       }),
+      invalidatesTags: ["Tasks"],
+    }),
+    addTask: builder.mutation({
+      query: (task) => ({
+        url: "/tasks",
+        method: "POST",
+        body: task,
+      }),
+      invalidatesTags: ["Tasks"],
     }),
   }),
 });
 
-export const { useGetTasksQuery, useUpdateTaskMutation } = baseApi;
+export const { useGetTasksQuery, useUpdateTaskMutation, useAddTaskMutation } =
+  baseApi;
 
 export default baseApi;
